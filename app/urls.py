4c881f887531fb7fd9c7dc2e088a475dbc8ab317
mrjana-123 <janarthanan.seltam@gmail.com>
2026-02-02 16:43:42 +0530

    stage

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('dashboard/', views.dashboard),
     path('get_keys', views.get_keys),
     path('key_generate', views.generate_key),
+    
     path('revoke_key', views.revoke_key),
+    
+    path('sender_revoke_key', views.sender_revoke_key),
+    
     path('activation_code', views.activation_code),
     path('current-user', views.current_user),
     path('update_profile', views.update_profile),
