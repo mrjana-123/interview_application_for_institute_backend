89310fffcda5cd23114ffc486df7fb83f5984592
mrjana-123 <janarthanan.seltam@gmail.com>
2026-02-03 17:52:23 +0530

    statges

--- app/views.py
+++ app/views.py
@@ -18,7 +18,7 @@ def create_key_expiry_notifications():
     target_date = today + timedelta(days=3)
 
     expiring_keys = Sender_Activation_code.objects.filter(
-        expiry_date__date=target_date,
+        expiry_date__gte=today,
         status="Active"
     )
 
